7f6c702086bcb34b162ebd255e0eb5f76da819cf
ellenupark <ellenupark@gmail.com>
2021-02-18 03:38:04 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import HomePage from './containers/HomePage'
+import ArticleContainer from './containers/ArticleContainer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h2>Header</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/articles/:id' component={ArticleContainer} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/containers/ArticleContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleContainer = () => {
+    return (
+        <div>
+            <h2>Articles</h2>
+        </div>
+    );
+};
+
+export default ArticleContainer;
