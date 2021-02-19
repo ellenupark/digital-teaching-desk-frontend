5622ad445a6611e8a36b022036e856cabaeb0f38
ellenupark <ellenupark@gmail.com>
2021-02-19 14:48:19 -0500

    Created live search function

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const SearchForm = () => {
+const SearchForm = (props) => {
     const [complexity, setComplexity] = useState('')
     const [resource, setResource] = useState([])
     const [minGroupSize, setMinGroupSize] = useState('')
@@ -15,14 +15,28 @@ const SearchForm = () => {
         // this.props.submitForm(this.state)
         // this.setState({itemName: ""})
     }
-  
-    const handleChange = (event, update) => {
+
+    const handleChange = (event, update, filterItem) => {
         let attribute = event.target.value
+        filterItem === 'complexity' && props.filterData(attribute, minGroupSize, maxGroupSize, resource, ageRange)
+        filterItem === 'min-group-size' && props.filterData(complexity, attribute, maxGroupSize, resource, ageRange)
+        filterItem === 'max-group-size' && props.filterData(complexity, minGroupSize, attribute, resource, ageRange)
         update(attribute)
     }
+  
+    // const handleChange = (event, update, filterItem) => {
+    //     let attribute = event.target.value
+    //     props.filterData(attribute, filterItem)
+    //     update(attribute)
+    // }
 
-    const handleCheckBoxChange = (event, setState, currentState) => {
+    const handleCheckBoxChange = (event, setState, currentState, filterItem) => {
         let attribute = event.target.name
+        // event.target.checked ? attribute = event.target.name : attribute = "";
+        let allAttributes = currentState.includes(attribute) ? currentState.filter(data => data !== attribute) : [...currentState, attribute]
+        filterItem === 'resource' && props.filterData(complexity, minGroupSize, maxGroupSize, allAttributes, ageRange)
+        filterItem === 'age' && props.filterData(complexity, minGroupSize, maxGroupSize, resource, allAttributes)
+        // props.filterData(attribute, filterItem)
         currentState.includes(attribute) ? setState(currentState.filter(data => data !== attribute)) : setState([...currentState, attribute])
     }
 
@@ -30,7 +44,8 @@ const SearchForm = () => {
         <Form>
             <Form.Group controlId="exampleForm.SelectCustomSizeSm">
                 <Form.Label>Complexity Level</Form.Label>
-                <Form.Control onChange={(event) => handleChange(event, setComplexity)} as="select" size="sm" custom>
+                <Form.Control onChange={(event) => handleChange(event, setComplexity, 'complexity')} as="select" size="sm" custom>
+                    <option>Any</option>
                     <option>Minimal Learning Curve</option>
                     <option>Moderate Learning Curve</option>
                     <option>Steep Learning Curve</option>
@@ -39,49 +54,50 @@ const SearchForm = () => {
             <Form.Group controlId="exampleForm.SelectCustomSizeSm">
                 <Form.Label>Resource Requirements</Form.Label><br />
                     <Form.Check 
-                        onChange={(event) => handleCheckBoxChange(event, setResource, resource)}
+                        onChange={(event) => handleCheckBoxChange(event, setResource, resource, 'resource')}
                         custom
                         inline
                         label="Laptop"
                         type='checkbox'
-                        id={`custom-inline-checkbox-1`}
+                        id={`resource-checkbox-1`}
                         name='Laptop'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setResource, resource)}
+                        onChange={(event) => handleCheckBoxChange(event, setResource, resource, 'resource')}
                         custom
                         inline
                         label="Phone"
                         type='checkbox'
-                        id={`custom-inline-checkbox-2`}
+                        id={`resource-checkbox-2`}
                         name='Phone'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setResource, resource)}
+                        onChange={(event) => handleCheckBoxChange(event, setResource, resource, 'resource')}
                         custom
                         inline
                         label="Headphones"
                         type='checkbox'
-                        id={`custom-inline-checkbox-3`}
+                        id={`resource-checkbox-3`}
                         name='Headphones'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setResource, resource)}
+                        onChange={(event) => handleCheckBoxChange(event, setResource, resource, 'resource')}
                         custom
                         inline
                         label="Tablet"
                         type='checkbox'
-                        id={`custom-inline-checkbox-4`}
+                        id={`resource-checkbox-4`}
                         name="Tablet"
                     />
             </Form.Group>
             <Form.Group controlId="exampleForm.SelectCustomSizeSm">
                 <Form.Label>Minimum Group Size</Form.Label>
-                <Form.Control  onChange={(event) => handleChange(event, setMinGroupSize)} as="select" size="sm" custom>
+                <Form.Control  onChange={(event) => handleChange(event, setMinGroupSize, 'min-group-size')} as="select" size="sm" custom>
                     <option>Any</option>
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
+                    <option>4</option>
                     <option>5</option>
                     <option>6</option>
                     <option>7</option>
@@ -90,11 +106,12 @@ const SearchForm = () => {
                     <option>10</option>
                 </Form.Control>
                 <Form.Label>Maximum Group Size</Form.Label>
-                <Form.Control  onChange={(event) => handleChange(event, setMaxGroupSize)} as="select" size="sm" custom>
+                <Form.Control  onChange={(event) => handleChange(event, setMaxGroupSize, 'max-group-size')} as="select" size="sm" custom>
                     <option>Any</option>
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
+                    <option>4</option>
                     <option>5</option>
                     <option>6</option>
                     <option>7</option>
@@ -106,52 +123,52 @@ const SearchForm = () => {
             <Form.Group controlId="exampleForm.SelectCustomSizeSm">
                 <Form.Label>Recommended Age</Form.Label><br />
                     <Form.Check 
-                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange)}
+                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange, 'age')}
                         custom
                         inline
                         label="1-3"
                         type='checkbox'
-                        id={`custom-inline-checkbox-1`}
+                        id={`age-checkbox-1`}
                         name='1-3'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange)}
+                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange, 'age')}
                         custom
                         inline
                         label="3-5"
                         type='checkbox'
-                        id={`custom-inline-checkbox-2`}
+                        id={`age-checkbox-2`}
                         name='3-5'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange)}
+                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange, 'age')}
                         custom
                         inline
                         label="5-8"
                         type='checkbox'
-                        id={`custom-inline-checkbox-3`}
+                        id={`age-checkbox-3`}
                         name='5-8'
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange)}
+                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange, 'age')}
                         custom
                         inline
                         label="9-12"
                         type='checkbox'
-                        id={`custom-inline-checkbox-4`}
+                        id={`age-checkbox-4`}
                         name="9-12"
                     />
                     <Form.Check
-                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange)}
+                        onChange={(event) => handleCheckBoxChange(event, setAgeRange, ageRange, 'age')}
                         custom
                         inline
                         label="13 and over"
                         type='checkbox'
-                        id={`custom-inline-checkbox-5`}
+                        id={`age-checkbox-5`}
                         name="13 and over"
                     />
             </Form.Group>
-            <Button variant="primary" size="lg">Submit</Button>{' '}
+            <Button type="submit" variant="primary" size="lg">Submit</Button>{' '}
             <Button variant="secondary" size="lg">Clear</Button>
         </Form>
         // <form onSubmit={(event) => this.handleSubmit(event)}>
--- src/containers/DirectoryContainer.js
+++ src/containers/DirectoryContainer.js
@@ -8,19 +8,65 @@ import Col from 'react-bootstrap/Col'
 const DirectoryContainer = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [searchData, setSearchData] = useState([]);
+
 
     useEffect(() => {
         fetch("http://localhost:3000/contents")
         .then((res) => res.json())
         .then((response) => {
             setData(response);
+            setSearchData(response)
             setIsLoading(false);
         })
         .catch((error) => console.log(error));
     }, []);
 
-    const filterData = () => {
-        
+    function filterData(complexity, minGroup, maxGroup, resource, age) {
+        let newData;
+        if (!!complexity && complexity !== 'Any') {
+            newData = data.filter(content => content.complexity_level === complexity)
+        } else if (complexity === 'Any') {
+            newData = data
+        }
+        if (!!minGroup && minGroup !== 'Any') {
+            newData !== undefined ? newData = newData.filter(content => parseInt(content.min_group_size) >= parseInt(minGroup)) : newData = data.filter(content => parseInt(content.min_group_size) >= parseInt(minGroup))
+        } else if (minGroup === 'Any') {
+            newData = data
+        }
+        if (!!maxGroup && maxGroup !== 'Any') {
+            newData !== undefined ? newData = newData.filter(content => content.max_group_size <= maxGroup) : newData = data.filter(content => content.max_group_size <= maxGroup)
+        } else if (maxGroup === 'Any') {
+            newData = data
+        }
+
+        if (resource.length > 0) {
+            newData !== undefined ? newData = newData.filter(content => resource.every(r=> content.resource_requirement.includes(r))) : newData = data.filter(content => resource.every(r=> content.resource_requirement.includes(r)))
+        }
+        if (age.length > 0) {
+            newData !== undefined ? newData = newData.filter(content => age.every(r=> content.age_range.includes(r))) : newData = data.filter(content => age.every(r=> content.age_range.includes(r)))
+        }
+        newData !== undefined ? setSearchData(newData) : setSearchData(data)
+
+        // switch (filterItem) {
+        //     case 'complexity':
+        //         value !== 'Any' ? setSearchData(data.filter(content => content.complexity_level >= value)) : setSearchData(data);
+        //         break;
+        //     case 'min-group-size':
+        //         value !== 'Any' ? setSearchData(data.filter(content => content.min_group_size >= value)) : setSearchData(data);
+        //         break;
+        //     case 'max-group-size':
+        //         value !== 'Any' ? setSearchData(data.filter(content => content.max_group_size <= value)) : setSearchData(data);
+        //         break;
+        //     case 'resource':
+        //         value ? setSearchData(data.filter(content => content.resource_requirement.includes(value))) : setSearchData(data);
+        //         break;
+        //     case 'age':
+        //         value ? setSearchData(data.filter(content => content.age_range.includes(value))) : setSearchData(data);
+        //         break;
+        //     default:
+        //         break;
+        // }
     }
 
     return (
@@ -29,10 +75,10 @@ const DirectoryContainer = () => {
                 <Container fluid="md">
                     <Row>
                         <Col>
-                            <SearchForm />
+                            <SearchForm filterData={filterData}/>
                         </Col>
                         <Col>
-                            <SearchList content={data} />
+                            <SearchList content={searchData} />
                         </Col>
                     </Row>
                 </Container> 
